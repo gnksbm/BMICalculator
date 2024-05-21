0b22470925abfb3652c07119ae1a0924dcc36378
Geonseob Kim <gnksbm@gmail.com>
2024-05-21 16:02:48 +0900

    Initial Commit

---
+++ SeSac_BMICalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSac_BMICalculator
+//
+//  Created by gnksbm on 5/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
