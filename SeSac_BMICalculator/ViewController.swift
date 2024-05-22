d80a7f94436d1878e0c4099171cfa9602152f794
Geonseob Kim <gnksbm@gmail.com>
2024-05-22 19:38:04 +0900

    분리된 객체 사용하며 VC 로직 수정

--- SeSac_BMICalculator/ViewController.swift
+++ SeSac_BMICalculator/ViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 final class ViewController: UIViewController {
+    var userNameForEdit: String?
     private let heightRange = 50...250
     private let weightRange = 3...250
     
@@ -17,6 +18,30 @@ final class ViewController: UIViewController {
     @IBOutlet var descriptionLabel: UILabel!
     @IBOutlet var randomButton: UIButton!
     @IBOutlet var calculateButton: UIButton!
+    
+    private lazy var nameButton = {
+        let button = UIButton()
+        button.setTitle("이름 변경", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.addTarget(
+            self,
+            action: #selector(editNameButtonTapped),
+            for: .touchUpInside
+        )
+        return button
+    }()
+    
+    private lazy var resetButton = {
+        let button = UIButton()
+        button.setTitle("리셋", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.addTarget(
+            self,
+            action: #selector(resetButtonTapped),
+            for: .touchUpInside
+        )
+        return button
+    }()
     
     private lazy var hiddenButton = {
         let button = UIButton()
@@ -35,23 +60,31 @@ final class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureLabels()
-        configureTextFields()
+        let user = User.fetchUser()
+        updateDescriptionLabelText(userName: user?.name)
+        configureTextFields(weight: user?.weight, height: user?.height)
         configureButtons()
+        configureNavigation()
     }
     
-    private func configureLabels() {
+    private func updateDescriptionLabelText(userName: String?) {
         let paragraphStyle = NSMutableParagraphStyle()
         paragraphStyle.lineSpacing = 5
+        let prefixMessage: String
+        if let userName {
+            prefixMessage = "\(userName)님"
+        } else {
+            prefixMessage = "당신"
+        }
         descriptionLabel.attributedText = .init(
-            string: "당신의 BMI 지수를\n알려드릴게요",
+            string: "\(prefixMessage)의 BMI 지수를\n알려드릴게요",
             attributes: [
                 .paragraphStyle: paragraphStyle
             ]
         )
     }
     
-    private func configureTextFields() {
+    private func configureTextFields(weight: Double?, height: Double?) {
         textFieldBackgroundViews.forEach {
             $0.layer.cornerRadius = 20
             $0.layer.borderWidth = 1
@@ -59,6 +92,12 @@ final class ViewController: UIViewController {
         weightTextField.rightView = hiddenButton
         weightTextField.rightViewMode = .always
         weightTextField.isSecureTextEntry = true
+        if let weight {
+            weightTextField.text = "\(weight)"
+        }
+        if let height {
+            heightTextField.text = "\(height)"
+        }
     }
     
     private func configureButtons() {
@@ -73,6 +112,66 @@ final class ViewController: UIViewController {
             action: #selector(calculateButtonTapped),
             for: .touchUpInside
         )
+    }
+    
+    private func configureNavigation() {
+        navigationItem.leftBarButtonItem = .init(customView: resetButton)
+        navigationItem.rightBarButtonItem = .init(customView: nameButton)
+    }
+    
+    private func showInvalidError(error: Error) {
+        let alertController = UIAlertController(
+            title: "잘못된 입력입니다",
+            message: error.localizedDescription,
+            preferredStyle: .alert
+        )
+        let doneAction = UIAlertAction(title: "확인", style: .destructive)
+        alertController.addAction(doneAction)
+        present(alertController, animated: true)
+    }
+    
+    @objc private func editNameFieldChanged(_ sender: UITextField) {
+        userNameForEdit = sender.text
+    }
+    
+    @objc private func editNameButtonTapped(_ sender: UIButton) {
+        let alertController = UIAlertController(
+            title: "변경할 이름을 입력해주세요",
+            message: nil,
+            preferredStyle: .alert
+        )
+        let savedUser = User.fetchUser()
+        let saveAction = UIAlertAction(
+            title: "저장",
+            style: .default
+        ) { _ in
+            User.updateUser(newName: self.userNameForEdit)
+            self.updateDescriptionLabelText(userName: self.userNameForEdit)
+        }
+        let cancelAction = UIAlertAction(
+            title: "취소",
+            style: .cancel
+        ) { _ in
+            self.userNameForEdit = nil
+        }
+        alertController.addTextField { textField in
+            textField.text = savedUser?.name
+            textField.addTarget(
+                self,
+                action: #selector(self.editNameFieldChanged),
+                for: .editingChanged
+            )
+        }
+        alertController.addAction(saveAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true)
+    }
+    
+    @objc private func resetButtonTapped(_ sender: UIButton) {
+        User.removeUser()
+        updateDescriptionLabelText(userName: nil)
+        weightTextField.text?.removeAll()
+        heightTextField.text?.removeAll()
     }
     
     @objc private func hiddenButtonTapped(_ sender: UIButton) {
@@ -98,46 +197,35 @@ final class ViewController: UIViewController {
     }
     
     @objc private func calculateButtonTapped(_ sender: UIButton) {
-        guard let weightText = weightTextField.text,
-              let weight = Double(weightText),
-              let heightText = heightTextField.text,
-              let height = Double(heightText)
-        else {
-            showInvalidInput(message: "숫자만 입력해주세요")
-            return
+        do {
+            let heightDouble = try NumericValidator.validateInput(
+                input: heightTextField.text,
+                range: 80..<250,
+                fieldName: "키"
+            )
+            let weightDouble = try NumericValidator.validateInput(
+                input: weightTextField.text,
+                range: 20..<300,
+                fieldName: "몸무게"
+            )
+            User.updateUser(
+                newWeight: weightDouble,
+                newHeight: heightDouble
+            )
+            let bmi = BMICalculator.calculate(
+                weight: weightDouble,
+                height: heightDouble
+            )
+            let alertController = UIAlertController(
+                title: "당신의 BMI 지수는?",
+                message: String(format: "%.1f", bmi) + "입니다",
+                preferredStyle: .alert
+            )
+            let doneAction = UIAlertAction(title: "확인", style: .cancel)
+            alertController.addAction(doneAction)
+            present(alertController, animated: true)
+        } catch {
+            showInvalidError(error: error)
         }
-        guard weightRange ~= Int(weight),
-              heightRange ~= Int(height)
-        else {
-            showInvalidInput(message: "정상적인 수를 입력해주세요")
-            return
-        }
-        let bmi = calculateBMI(
-            height: height,
-            weight: weight
-        )
-        let alertController = UIAlertController(
-            title: "당신의 BMI 지수는?",
-            message: String(format: "%.1f", bmi) + "입니다",
-            preferredStyle: .alert
-        )
-        let doneAction = UIAlertAction(title: "확인", style: .cancel)
-        alertController.addAction(doneAction)
-        present(alertController, animated: true)
-    }
-    
-    private func showInvalidInput(message: String) {
-        let alertController = UIAlertController(
-            title: "잘못된 입력입니다",
-            message: message,
-            preferredStyle: .alert
-        )
-        let doneAction = UIAlertAction(title: "확인", style: .destructive)
-        alertController.addAction(doneAction)
-        present(alertController, animated: true)
-    }
-    
-    private func calculateBMI(height: Double, weight: Double) -> Double {
-         weight / (height * height) * 10000
     }
 }
